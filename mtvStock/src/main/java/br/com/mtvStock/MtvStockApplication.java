c6cf343c1ad151d97550f56888b7a4b99219891c
jose-matheusc <josematheus112@gmail.com>
2024-11-30 16:02:02 -0300

    initial commit

---
+++ mtvStock/src/main/java/br/com/mtvStock/MtvStockApplication.java
@@ -0,0 +1,13 @@
+package br.com.mtvStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtvStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtvStockApplication.class, args);
+	}
+
+}
---
+++ mtvStock/src/test/java/br/com/mtvStock/MtvStockApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mtvStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtvStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
