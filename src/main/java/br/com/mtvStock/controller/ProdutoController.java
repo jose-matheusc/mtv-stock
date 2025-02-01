d8ea3a8572aaf8046918ece1e4ef9dfd3de3d2bb
jose-matheusc <josematheus112@gmail.com>
2025-02-01 20:43:12 -0300

    fix: remove comments

--- src/main/java/br/com/mtvStock/controller/ProdutoController.java
+++ src/main/java/br/com/mtvStock/controller/ProdutoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/product/v1") // Define o prefixo para os endpoints
+@RequestMapping("/product/v1")
 @RequiredArgsConstructor
 public class ProdutoController {
 
